6b32e5ba9930121a1230807008fdf8634661fffb
Luke Mujica <lukemujica@gmail.com>
2017-09-27 19:31:37 -0700

    Done

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,16 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! {|x| x.to_i}
+    a.map! {|x| x+2}
+    a.keep_if {|x| x%2 == 0}
+    a.uniq!
+    a.delete_if {|x| x > 10}
+    total = 0
+    a.each do |x|
+      total += x
+    end
+    return total
   end
+  
 end
-
-
